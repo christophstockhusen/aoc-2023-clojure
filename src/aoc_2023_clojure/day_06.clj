2156ba9c07ecbd9149794b9a808842e2786edcaf
Christoph Stockhusen <mail@christophstockhusen.de>
2023-12-07 10:39:23 +0100

    Day 06 using quadratic equation

--- src/aoc_2023_clojure/day_06.clj
+++ src/aoc_2023_clojure/day_06.clj
@@ -1,32 +1,43 @@
 (ns aoc-2023-clojure.day-06
   (:require
    [clojure.java.io :as io]
-   [clojure.string :as str]))
-
-(def input "Time:      7  15   30
-Distance:  9  40  200")
+   [clojure.string :as str]
+   [clojure.math :as math]))
 
 (defn- parse-input [part input]
-  (if (= 1 part)
+  (cond
+    (= 1 part)
     (let [numbers (map parse-long (re-seq #"\d+" input))
           [times distances] (split-at (/ (count numbers) 2) numbers)]
-      (map #(hash-map :time %1 :record %2) times distances))
-    (let [digit-lines (map (fn [l] (re-seq #"\d" l)) (str/split-lines input))
-          numbers (map parse-long (map str/join digit-lines))]
-      [{:time (first numbers)
-        :record (second numbers)}])))
+      (map #(hash-map :racing-time %1 :record-distance %2) times distances))
 
-(defn- distance [charged max-time]
-  (* charged (- max-time charged)))
+    :else
+    (let [digit-lines (map #(re-seq #"\d" %) (str/split-lines input))
+          [time record] (map (comp parse-long str/join) digit-lines)]
+      [{:racing-time time :record-distance record}])))
 
-(defn- max-distances [time]
-  (map #(distance % time) (range time)))
+;; Distance"" charged * (max-time - charged) = - charged^2 + max-time * charged
+;; We search for natural numbers with - charged^2 + max-time * charged > record, i.e.
+;; natural numbers _between_ the roots of - charged^2 + max-time * charged - record = 0
+;; or charged^2 - max-time * charged + record = 0
+
+(defn- roots [p q]
+  (let [r (math/sqrt (- (math/pow (/ p 2) 2) q))]
+    [(- (- (/ p 2)) r) (+ (- (/ p 2)) r)]))
+
+(defn- ugly-floor [x]
+  (math/floor (- x 0.000001)))
+
+(defn- ugly-ceil [x]
+  (math/ceil (+ x 0.000001)))
+
+(defn- winning-options [{racing-time :racing-time
+                         record-distance :record-distance}]
+  (let [[r1 r2] (roots (- racing-time) record-distance)]
+    (int (inc (- (ugly-floor r2) (ugly-ceil r1))))))
 
 (defn- margin-of-error [races]
-  (->> (map max-distances (map :time races))
-       (map (fn [r ds] (filter #(< (:record r) %) ds)) races)
-       (map count)
-       (reduce *)))
+  (reduce * (map winning-options races)))
 
 (defn a
   ([] (a (slurp (io/resource "06.txt"))))
